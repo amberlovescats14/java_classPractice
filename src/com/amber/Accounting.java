821c974b984393e298f517b510337c07eec27bf6
Amber Jones <amberlovescats14@gmail.com>
2019-09-20 14:55:00 -0500

    trying to fix this repo

---
+++ src/com/amber/Accounting.java
@@ -0,0 +1,50 @@
+package com.amber;
+
+public class Accounting {
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    private String customerPhoneNumber;
+}
